1e7cdaad366076097e2ae0bcc3900039164ada3c
Violet <eerfeer@yandex.ru>
2022-01-31 20:11:34 +0300

    Module №2 tasks solved

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,6 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (const key in salaries)
+    sum += [NaN, Infinity, -Infinity].includes(+salaries[key]) ? 0 : salaries[key];
+  return sum;
 }
